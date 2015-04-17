ac8d147f5025099e3e743869b24044376593db18
Hamish Hutchings <hamhut1066@gmail.com>
2015-04-17 19:49:11 +0100

    move to subdir

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,8 @@
-#!/usr/bin/env python
-
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
-
-@app.route("/")
+@app.route('/')
 def hello():
-    return "Hello World"
-
-if __name__ == "__main__":
-    app.run()
+        return 'Hello World!'
