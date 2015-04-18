e8617a29596a3034bb727e178c38bb43a4cfbf09
Hamish Hutchings <hamhut1066@gmail.com>
2015-04-19 00:29:56 +0100

    Add committing of posts, and fetching of posts

--- karma/api/post.py
+++ karma/api/post.py
@@ -2,12 +2,22 @@
 
 from flask import request
 import json
-from flask.ext.login import current_user
 from flask.ext.restful import Resource
 from karma.api.models.post import Post as P
+from karma.models import User
+from karma import db
 
 
 class Post(Resource):
+
+    def __init__(self, title=None, content=None, user_id=None):
+        if title:
+            self.title = title
+        if content:
+            self.content = content
+        if user_id:
+            self.user_id = user_id
+
     def get(self, post_id):
         p = P.query.filter_by(id=post_id).first_or_404()
 
@@ -36,6 +46,20 @@ class Posts(Resource):
     def put(self):
         try:
             obj = json.loads(request.data)
+            user = User.query.get(obj['user'])
         except ValueError:
             return {"status": 400}
-        
+
+        # create post.
+        p = P(title=obj['data']['title'],
+                    content=obj['data']['content'],
+                    user_id=user.id)
+
+        db.session.add(p)
+        db.session.commit()
+
+        return {
+            "id": p.id,
+            "title": p.title,
+            "content": p.content,
+            "user_id": p.user_id}
